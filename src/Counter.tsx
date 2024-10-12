1fd3828aeb347f5e1c42d6c2bc31a9bb43fedfeb
Noriyo Akita <noriyo.akita@gmail.com>
2024-10-12 23:28:27 +0900

    Use update function to increment or decrement

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,21 +2,25 @@ import { useState } from "react";
 
 export function Counter() {
   const [count, setCount] = useState(0);
+  // const increment = () => {
+  //   setCount((currentValue) => currentValue + 1);
+  // }
+
   const increment = () => {
-    setCount(count + 1);
+    setTimeout(() => {
+      setCount((currentValue) => currentValue + 1);
+    }, 2000);
   }
 
-  // const increment = () => {
-  //   setTimeout(() => {
-  //     setCount(count + 1);
-  //   }, 2000);
-  // }
+  const decrement = () => {
+    setCount((currentValue) => currentValue - 1);
+  }
 
   return (
     <>
       count is {count}
       <button onClick={increment}>+</button>
-      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={decrement}>-</button>
       <button onClick={() => setCount(0)}>Reset</button>
     </>
   )
