f0cfb6831cefdb3fabf0397cdca3adf83f700f32
noriyotcp <noriyo.akita@gmail.com>
2024-10-14 14:26:56 +0900

    Pass local and format options

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 
 export function Clock() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }));
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
     setTimeout(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(
+        new Date().toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false,
+        })
+      );
     }, 1000, signal);
 
     return () => controller.abort();
