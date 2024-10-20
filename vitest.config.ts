f59abf7c3c379871adc6399cf48dd3a867064c99
noriyotcp <noriyo.akita@gmail.com>
2024-10-21 02:29:21 +0900

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
-export default {
-  testFilePatterns: ['src/**/*.test.tsx'],
-  testRunnerOptions: {
-    reporter: 'spec',
-    timeout: 5000
-  }
-};
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+});
+
